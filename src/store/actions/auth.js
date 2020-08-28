5a522be729440a348c7e1d7f855c847567560c25
shawnmorreau <shawn.morreau@hotmail.com>
2020-08-28 00:57:13 -0400

    frontend

---
+++ src/store/actions/auth.js
@@ -0,0 +1,40 @@
+import {apiCall, setTokenHeader} from "../../services/api";
+import {SET_CURRENT_USER} from "../actionTypes";
+import {addError, removeError} from "./errors";
+export function setCurrentUser(user){
+    return{
+        type: SET_CURRENT_USER,
+        user
+    }
+};
+export function logout(){
+    return dispatch =>{
+        localStorage.clear();
+        setAuthorizationToken(false);
+        dispatch(setCurrentUser({}));
+    }
+}
+export function setAuthorizationToken(token){
+    setTokenHeader(token);
+}
+export function authUser(type,userData){
+    return dispatch=>{
+        return new Promise((resolve,reject)=>{
+            return apiCall("post",`/api/auth/${type}`,userData)
+                    .then(({token,...userData})=>{
+                        localStorage.setItem("jwtToken",token);
+                        setAuthorizationToken(token);
+                        dispatch(setCurrentUser(userData)); //store.dispatch(action(type));
+                        dispatch(removeError());
+                        resolve();
+                    })
+                    .catch(err=>{
+                        dispatch(addError(err));
+                        reject("It's not you, it's us.");
+                    })
+        })
+        .catch(err=>{
+            return err;
+        })
+    }
+}
---
+++ src/store/reducers/index.js
@@ -0,0 +1,12 @@
+//rootreducer
+import {combineReducers} from "redux";
+import currentUser from "./currentUser";
+import errors from "./errors";
+import messages from "./messages";
+
+const rootReducer = combineReducers({
+    currentUser,
+    errors,
+    messages
+});
+export default rootReducer;
